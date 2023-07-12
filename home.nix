fa9cdf49214803489d9a00adbf0ace8b19c99e70
Sootpaws <humannum14916@gmail.com>
2023-07-12 01:22:13 +0000

    Add comments to home.nix

--- home.nix
+++ home.nix
@@ -1,14 +1,21 @@
+# Home Manager configuration for the primary user
+
 primaryUser: { config, pkgs, ... }: {
+    # Keep stateful data compatible with this version
     home.stateVersion = "23.05";
+    # Allow Home Manager to manage itself
     programs.home-manager.enable = true;
 
+    # General info
     home.username = primaryUser;
     home.homeDirectory = "/home/" + primaryUser;
 
+    # Install-only packages
     home.packages = [
         pkgs.neofetch
     ];
 
+    # Git config
     programs.git = {
         enable = true;
 
