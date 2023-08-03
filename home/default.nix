00cf9db832a0a55b90f894232c7be845ec305bf2
Sootpaws <humannum14916@gmail.com>
2023-08-03 16:16:46 -0700

    [home] Split Librewolf configuration into its own file

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,7 @@
         ./starship
         ./micro
         ./git
+        ./librewolf
     ];
 
     # Keep stateful data compatible with this version
@@ -31,22 +32,4 @@
         pkgs.du-dust
         pkgs.neofetch
     ];
-
-    # Librewolf, web browser
-    programs.librewolf = {
-        enable = true;
-        settings = {
-            "privacy.clearOnShutdown.history" = false;
-            "browser.sessionstore.resume_from_crash" = false;
-            "browser.warnOnQuitShortcut" = false;
-            "browser.startup.homepage" = "chrome://browser/content/blanktab.html";
-            "browser.newtabpage.enabled" = false;
-            "browser.search.hiddenOneOffs" = "DuckDuckGo,DuckDuckGo Lite,MetaGer,SearXNG - searx.be,StartPage,Wikipedia (en)";
-            "browser.urlbar.shortcuts.bookmarks" = false;
-            "browser.urlbar.shortcuts.history" = false;
-            "browser.urlbar.shortcuts.tabs" = false;
-            "browser.toolbars.bookmarks.visibility" = "always";
-            "browser.bookmarks.addedImportButton" = true;
-        };
-    };
 }
