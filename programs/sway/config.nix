57fdacd353b21dd92109a017e4e3c5fddbaca879
Sootpaws <sootpaws@proton.me>
2024-05-08 00:31:34 -0400

    Alphabetize sway open keybinds and add Factorio open keybind

--- programs/sway/config.nix
+++ programs/sway/config.nix
@@ -21,13 +21,14 @@ set $down k
 # Switch to open mode
 bindsym $mod+o mode "open"
 mode "open" {
-    bindsym m exec dmenu_path | dmenu | xargs swaymsg exec --, mode "default"
-    bindsym t exec $term, mode "default"
-    bindsym w exec librewolf, mode "default"
     bindsym a exec tor-browser, mode "default"
+    bindsym f exec factorio, mode "default"
     bindsym k exec keepassxc, mode "default"
     bindsym l exec libreoffice, mode "default"
+    bindsym m exec dmenu_path | dmenu | xargs swaymsg exec --, mode "default"
     bindsym r exec krita, mode "default"
+    bindsym t exec $term, mode "default"
+    bindsym w exec librewolf, mode "default"
     bindsym v exec VirtualBox, mode "default"
     bindsym F4 exec $term --command $music_script select, mode "default"
 
