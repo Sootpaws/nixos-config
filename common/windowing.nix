10d043a9aef3dc75a3bf8ce13531460c19738790
Sootpaws <sootpaws@proton.me>
2024-11-22 21:18:32 +0000

    Define general windowing controls in a separate file

---
+++ common/windowing.nix
@@ -0,0 +1,39 @@
+# Basic windowing controls
+{ modifier }: let
+        directions = [ "Left" "Right" "Up" "Down" ];
+        tabs = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" ];
+        bind = keys: action: { keys = modifier ++ keys; inherit action; };
+    in {
+        normal =
+            # Move focus
+            (map (direction:
+                bind [ direction ] { type = "moveFocus"; inherit direction; }
+            ) directions) ++
+            # Move window
+            (map (direction:
+                bind [ "Shift" direction ] { type = "moveWindow"; inherit direction; }
+            ) directions) ++
+            # Move focus tab
+            (map (tab:
+                bind [ tab ] { type = "moveFocusTab"; inherit tab; }
+            ) tabs) ++
+            # Move window tab
+            (map (tab:
+                bind [ "Shift" tab ] { type = "moveWindowTab"; inherit tab; }
+            ) tabs) ++
+            [
+                # Fullscreen
+                (bind [ "f" ] { type = "fullscreen"; })
+                # Kill focused
+                (bind [ "Shift" "q" ] { type = "killFocused"; })
+                # Modes
+                (bind [ "r" ] { type = "mode"; mode = "resize"; })
+            ];
+        resize = [
+            (bind [ "Left" ] { type = "resize"; axis = "width"; direction = "grow"; })
+            (bind [ "Right" ] { type = "resize"; axis = "width"; direction = "shrink"; })
+            (bind [ "Up" ] { type = "resize"; axis = "height"; direction = "shrink"; })
+            (bind [ "Down" ] { type = "resize"; axis = "height"; direction = "grow"; })
+            (bind [ "Escape" ] { type = "mode"; mode = "normal"; })
+        ];
+    }
