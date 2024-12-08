8552d036c462881dfd7a1fed7c0b1f84db499b9c
Sootpaws <sootpaws@proton.me>
2024-12-08 15:12:22 +0000

    Allow configuring the Zellij modifier keybind

--- programs/zellij/default.nix
+++ programs/zellij/default.nix
@@ -1,48 +1,56 @@
-{ pkgs, config, ... }: let
-    modifier = [ "Super" "Ctrl" ];
-    # Use common windowing keybinds
-    keybinds = import ../../common/windowing.nix { inherit modifier; };
-    bindName = keys: "bind \"${ (toString keys) }\"";
-    formatBinds = bindings: builtins.listToAttrs (map (binding: with binding; {
-        name = bindName (map (key:
-                if key == "Escape" then "Esc" else key
-            ) keys);
-        value = {
-            moveFocus = { MoveFocus = action.direction; };
-            moveWindow = { MovePane = action.direction; };
-            moveFocusTab = { GoToTab = action.tab; };
-            moveWindowTab = { }; # TODO
-            fullscreen = { ToggleFocusFullscreen = []; };
-            killFocused = { CloseFocus = []; };
-            mode = { SwitchToMode = action.mode; };
-            resize = { Resize = {
-                "grow width" = "Right";
-                "grow height" = "Down";
-                "shrink width" = "Left";
-                "shrink height" = "Up";
-            }.${action.direction + " " + action.axis}; };
-        }.${action.type};
-    }) bindings);
-in {
-    programs.zellij = {
-        enable = true;
-        settings = {
-            ui.pane_frames.hide_session_name = true;
-            default_shell = config.home.sessionVariables.SHELL;
-            session_serialization = false;
-            keybinds = {
-                _props = { clear-defaults = true; };
-                normal = formatBinds keybinds.normal // {
-                    ${bindName (modifier ++ [ "o" ])} = { NewPane = []; };
-                    ${bindName (modifier ++ [ "t" ])} = { NewTab = []; };
-                };
-                resize = formatBinds keybinds.resize;
-            };
+{ pkgs, lib, config, ... }: {
+    options = {
+        programs.zellij.config.modifier = lib.mkOption {
+            type = lib.types.listOf lib.types.string;
+            default = [ "Super" "Ctrl" ];
         };
     };
-    xdg.configFile.zellijLayouts = {
-        source = ./layouts;
-        recursive = true;
-        target = "zellij/layouts";
+    config = let
+        modifier = config.programs.zellij.config.modifier;
+        # Use common windowing keybinds
+        keybinds = import ../../common/windowing.nix { inherit modifier; };
+        bindName = keys: "bind \"${ (toString keys) }\"";
+        formatBinds = bindings: builtins.listToAttrs (map (binding: with binding; {
+            name = bindName (map (key:
+                    if key == "Escape" then "Esc" else key
+                ) keys);
+            value = {
+                moveFocus = { MoveFocus = action.direction; };
+                moveWindow = { MovePane = action.direction; };
+                moveFocusTab = { GoToTab = action.tab; };
+                moveWindowTab = { }; # TODO
+                fullscreen = { ToggleFocusFullscreen = []; };
+                killFocused = { CloseFocus = []; };
+                mode = { SwitchToMode = action.mode; };
+                resize = { Resize = {
+                    "grow width" = "Right";
+                    "grow height" = "Down";
+                    "shrink width" = "Left";
+                    "shrink height" = "Up";
+                }.${action.direction + " " + action.axis}; };
+            }.${action.type};
+        }) bindings);
+    in {
+        programs.zellij = {
+            enable = true;
+            settings = {
+                ui.pane_frames.hide_session_name = true;
+                default_shell = config.home.sessionVariables.SHELL;
+                session_serialization = false;
+                keybinds = {
+                    _props = { clear-defaults = true; };
+                    normal = formatBinds keybinds.normal // {
+                        ${bindName (modifier ++ [ "o" ])} = { NewPane = []; };
+                        ${bindName (modifier ++ [ "t" ])} = { NewTab = []; };
+                    };
+                    resize = formatBinds keybinds.resize;
+                };
+            };
+        };
+        xdg.configFile.zellijLayouts = {
+            source = ./layouts;
+            recursive = true;
+            target = "zellij/layouts";
+        };
     };
 }
