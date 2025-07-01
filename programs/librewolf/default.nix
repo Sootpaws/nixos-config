98a3f295673f0ce86fc8062c3899102c62f231d2
Sootpaws <sootpaws@proton.me>
2025-07-01 10:31:49 -0700

    Browser search config

--- programs/librewolf/default.nix
+++ programs/librewolf/default.nix
@@ -20,6 +20,28 @@ in {
                 "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
                 "webgl.disabled" = false;
                 "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+                "browser.search.separatePrivateDefault" = false;
+                "browser.urlbar.suggest.recentsearches" = false;
+                "browser.urlbar.suggest.bookmark" = false;
+                "browser.urlbar.suggest.engines" = false;
+                "browser.urlbar.suggest.history" = false;
+                "browser.urlbar.suggest.openpage" = false;
+                "browser.urlbar.suggest.topsites" = false;
+                "browser.urlbar.shortcuts.bookmarks" = false;
+                "browser.urlbar.shortcuts.history" = false;
+                "browser.urlbar.shortcuts.tabs" = false;
+            };
+            search = {
+                default = "ddgn";
+                engines = {
+                    ddgn = {
+                        name = "DuckDuckNo";
+                        urls = [{
+                            template = "https://noai.duckduckgo.com/?q={searchTerms}";
+                        }];
+                    };
+                };
+                force = true;
             };
             userContent = builtins.readFile ./captcha.css;
         };
