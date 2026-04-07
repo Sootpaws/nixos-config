a4dddd8c8d5dc666aa77cf42dd3bc8b84c2e826d
Sootpaws <sootpaws@proton.me>
2026-04-07 12:52:25 -0400

    Update laptop home version

--- hardware/laptop/default.nix
+++ hardware/laptop/default.nix
@@ -1,11 +1,8 @@
 # Hardware and basic system configuration for my laptop
 
 { ... }: {
-    # Keep any system-level state compatible with this version
     system.stateVersion = "24.05";
-
-    # Keep any user-level state compatible with this version
-    home-manager.users.primary.home.stateVersion = "23.05";
+    home-manager.users.primary.home.stateVersion = "26.05";
 
     # System architecture
     nixpkgs.hostPlatform = "x86_64-linux";
